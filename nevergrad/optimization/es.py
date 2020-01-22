66efd827944be41bc14c49ba7693eda36f3b8525
Jeremy Rapin <jrapin@fb.com>
2020-01-22 16:00:53 +0100

    Fixes

--- nevergrad/optimization/es.py
+++ nevergrad/optimization/es.py
@@ -1,5 +1,6 @@
 import warnings
 import typing as tp
+# import numpy as np
 from nevergrad.parametrization import parameter as p
 from nevergrad.common.tools import OrderedSet
 from . import base
@@ -27,6 +28,14 @@ class _EvolutionStrategy(base.Optimizer):
             uid = next(iter(self._asked_queue))
         param = self._population[uid].spawn_child()
         param.mutate()
+        # if self._parameters.de_step and len(self._population) > 1:
+        #     sdata = [self._population[s].get_standardized_data(reference=self.instrumentation)
+        #              for s in self._rng.choice(list(self._population), 2, replace=False)]
+        #     F1, F2 = 0.8, 0.8
+        #     data = param.get_standardized_data(reference=self.instrumentation)
+        #     data += F2 * (self.current_bests["pessimistic"].x - data)
+        #     #data += F1 * (sdata[1] - sdata[0])
+        #     param.set_standardized_data(data, reference=self.instrumentation)
         if self._parameters.recombinations:
             selected = self._rng.choice(list(self._population), self._parameters.recombinations, replace=False)
             param.recombine(*(self._population[s] for s in selected))
@@ -40,10 +49,12 @@ class _EvolutionStrategy(base.Optimizer):
             parent_value = float('inf') if uid not in self._population else self._population[uid]._meta["value"]
             if value < parent_value:
                 self._population[uid] = candidate
+            self._told_queue.append(uid)
         else:
-            uid = candidate.parents_uids[0]
+            uid = candidate.uid
             if candidate.parents_uids[0] not in self._population and len(self._population) < self._parameters.popsize:
                 self._population[uid] = candidate
+                self._told_queue.append(uid)
             else:
                 self._waiting.append(candidate)
             if len(self._waiting) >= self._parameters.offsprings:
@@ -51,8 +62,9 @@ class _EvolutionStrategy(base.Optimizer):
                 choices.sort(key=lambda x: x._meta["value"])
                 self._population = {x.uid: x for x in choices[:self._parameters.popsize]}
                 self._told_queue.clear()
+                self._asked_queue.clear()
+                self._waiting.clear()
                 self._told_queue.extend(list(self._population))
-        self._told_queue.append(uid)
 
 
 class EvolutionStrategy(base.ParametrizedFamily):
@@ -66,6 +78,7 @@ class EvolutionStrategy(base.ParametrizedFamily):
             popsize: int = 40,
             offsprings: tp.Optional[int] = None,
             only_offsprings: bool = False,
+            # de_step: bool = False,
     ) -> None:
         assert offsprings is None or not only_offsprings or offsprings > popsize
         if only_offsprings:
@@ -74,6 +87,7 @@ class EvolutionStrategy(base.ParametrizedFamily):
         self.popsize = popsize
         self.offsprings = offsprings
         self.only_offsprings = only_offsprings
+        # self.de_step = de_step
         super().__init__()
 
 
